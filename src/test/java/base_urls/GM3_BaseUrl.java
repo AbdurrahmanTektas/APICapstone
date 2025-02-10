2764516d4d43c3c545462e4fce60077d0177f4bc
abdurrahman <abdr.tkts@gmail.com>
2025-02-10 12:30:50 +0100

    1

---
+++ src/test/java/base_urls/GM3_BaseUrl.java
@@ -0,0 +1,34 @@
+package base_urls;
+
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.specification.RequestSpecification;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.BeforeSuite;
+
+public class GM3_BaseUrl {
+
+    protected static RequestSpecification spec;
+
+    @BeforeSuite
+    public void setSpec(){
+
+            WebDriver driver = new ChromeDriver(new ChromeOptions().addArguments("--headless"));
+            driver.get("https://qa-gm3.quaspareparts.com/");
+            driver.findElement(By.linkText("Login")).click();
+            driver.findElement(By.id("username")).sendKeys("yasin@clarusway.com");
+            driver.findElement(By.id("password")).sendKeys("123456.Aa" + Keys.ENTER);
+            String gSessionId = driver.manage().getCookieNamed("GSESSIONID").getValue();
+            System.out.println(gSessionId);
+            driver.quit();
+
+            spec = new RequestSpecBuilder()
+                    .setBaseUri("https://qa-gm3.quaspareparts.com/a3m/auth/api")
+                    .addHeader("Cookie", "GSESSIONID=" + gSessionId)
+                    .build();
+
+}
+}
---
+++ src/test/java/requests/CountryService.java
@@ -0,0 +1,29 @@
+package requests;
+
+import base_urls.GM3_BaseUrl;
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.hamcrest.Matchers;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class CountryService extends GM3_BaseUrl {
+    @Test
+    void getCountries(){
+
+        Response response= RestAssured.given(spec).get("/country");
+
+
+        System.out.println(response.statusCode());
+        System.out.println(response.getStatusLine());
+        response.prettyPrint();
+
+        response.then()
+                .statusCode(200)
+                .body("name",Matchers.equalTo("Germany"),
+                        "currncy", Matchers.equalTo("EUR"));
+}
+}
