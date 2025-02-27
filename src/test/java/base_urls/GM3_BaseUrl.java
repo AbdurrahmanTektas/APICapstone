8a628c92de66a1527c138fb25fbdbe532b3865ca
abdurrahman <abdr.tkts@gmail.com>
2025-02-27 10:47:52 +0100

    Allure report

--- src/test/java/base_urls/GM3_BaseUrl.java
+++ src/test/java/base_urls/GM3_BaseUrl.java
@@ -7,7 +7,10 @@ import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
+
+import java.io.IOException;
 
 public class GM3_BaseUrl {
 
@@ -29,5 +32,27 @@ public class GM3_BaseUrl {
                 .setBaseUri("https://qa-gm3.quaspareparts.com/a3m/auth/api")
                 .addHeader("Cookie", "GSESSIONID=" + gSessionId)
                 .build();
+    }
+/*
+    @AfterSuite
+    public void executeReport() throws IOException, InterruptedException {
+        // Allure komutunu çalıştır
+        Thread.sleep(3000);
+        ProcessBuilder processBuilder = new ProcessBuilder("allure", "serve", "allure-results");
+        processBuilder.inheritIO();
+        // Konsol çıktısını göster
+//        Process başlat
+        Process process = processBuilder.start();
+        // Process'in tamamlanmasını bekle
+        process.waitFor();
+
+
+    }
+
+ */
+
+    @AfterSuite
+    public void generateAllureReport() throws IOException {
+        new ProcessBuilder().command("cmd.exe", "/c", "allure serve").start();
     }
 }
