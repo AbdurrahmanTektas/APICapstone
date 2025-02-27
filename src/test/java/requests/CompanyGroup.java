e0dde73e1e91631e4980298ea2342edd7664dc8a
abdurrahman <abdr.tkts@gmail.com>
2025-02-27 09:45:57 +0100

    update 4

--- src/test/java/requests/CompanyGroup.java
+++ src/test/java/requests/CompanyGroup.java
@@ -4,27 +4,25 @@ import base_urls.GM3_BaseUrl;
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
 import org.hamcrest.Matchers;
-import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
 import static org.testng.Assert.assertEquals;
 
-public class CountryService extends GM3_BaseUrl {
+public class CompanyGroup extends GM3_BaseUrl {
     @Test
-    void getCountries(){
+    void getCompanyGroup(){
 
         Response response = RestAssured
-                .given(spec).get("/country/DE");
+                .given(spec).get("/organization-group?content=full");
         System.out.println(response.statusCode());
         System.out.println(response.getStatusLine());
         response.prettyPrint();
 
         response.then().statusCode(200)
-                .body("name", Matchers.equalTo("Germany"),
-                "currency",Matchers.equalTo("EUR"));
+                .body("[0].id", Matchers.equalTo(2),
+                "[0].name", Matchers.equalTo("Acme Company Group Edited"))
+                .body("[0].short_name", Matchers.equalTo("Acme CG"));
+
         assertEquals(response.statusCode(), 200);
-}
-}
+    }
+    }
